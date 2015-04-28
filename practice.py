450493e0455bd4779786042e030ffc6b3e7ece08
nikostan <nikostan@users.noreply.github.com>
2015-04-27 23:05:13 -0700

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,19 +1,14 @@
 # /usr/bin/python
-
 print ("it's nice " + 'to be "or not to be"')
 # it's nice to be "or not to be"
-
 print ('where aren\'t you?')
 # where aren't you?
-
 print ('hello\nworld')
 # hello
 # world
-
 print ('''hello
 ... \tworld''')
 # hello
 # 	world
-
 print ('go to \\news')
 # go to \news
